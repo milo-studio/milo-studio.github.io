49cf267e086c891c6c8854b60dfa973cb2361071
Nicola Milo <nicola.milo1@gmail.com>
2026-04-02 12:19:05 +0200

    Add Landscape

---
+++ script.js
@@ -0,0 +1,38 @@
+// Inizializzazione AOS
+AOS.init();
+
+// Configurazione Particles.js
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.2, random: false },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.1,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+});
